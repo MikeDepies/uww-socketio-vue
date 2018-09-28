517fcf58c989d623ed1efce1f3dc5bebcabefbaa
Michael  Depies <mdepies@blackthorne.local>
2018-09-28 00:04:59 -0500

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
-
+import VueSocketio from 'vue-socket.io';
+Vue.use(VueSocketio, 'http://localhost:3000');
 new Vue({
   router,
   render: h => h(App)
